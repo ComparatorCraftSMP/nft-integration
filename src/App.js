af160879782bada84e835fc2cf1105f8e01b1af8
BossDaily <comparatorcraft2020@gmail.com>
2022-03-27 17:52:19 -0500

    I forgor to commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react'
+import Card from './Card';
 
+
+const IMAGES = [
+  "https://cdn.discordapp.com/attachments/762750140250718220/927660115371360286/2022-01-03_14.29.55.png",
+  "https://media.discordapp.net/attachments/762750140250718220/927660116231200808/2022-01-03_14.29.28.png?width=1693&height=910",
+  "https://media.discordapp.net/attachments/762750140250718220/927660116893925416/2022-01-03_14.15.17.png?width=1693&height=910",
+  "https://media.discordapp.net/attachments/762750140250718220/927660414681100310/2022-01-03_14.31.12.png?width=1693&height=910",
+  "https://media.discordapp.net/attachments/857730336679002172/944726199203155978/unknown.png"
+]
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return ( 
+    <div>
+      {IMAGES.map(image => (
+          <Card source={image} key={image} />
+        ))}
     </div>
+    
   );
 }
 
--- src/Card.js
+++ src/Card.js
@@ -1,7 +1,43 @@
 import React from 'react';
 
-const Card = () => {
-
+const Card = ({ imgSRC }) => {
+    return (
+        <div className="col">
+            <div className="card shadow-sm" data-price="350.95">
+                <img src={imgSRC} className="card-img-top" width="100%" alt={Math.floor(Math.random(0,11000))} />
+                <div className="card-body">
+                    <h5 className="card-text">Own Mahoose from UltraVanilla as a unique digital asset!</h5>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Attribute</th>
+                                <th scope="col">Value</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">Vibes</th>
+                                <td>100 / 100</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">Swag level</th>
+                                <td>100 / 100</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div className="purchase-zone">
+                        <h4 className="card-text"><strong>Price:</strong> 350.95 ETH</h4>
+                        <div className="d-flex justify-content-between align-items-center">
+                            <div className="btn-group">
+                                <button type="button" className="btn btn-primary">Purchase this NFT</button>
+                            </div>
+                            <small className="text-muted progress"></small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Card
